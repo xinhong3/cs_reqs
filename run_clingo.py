40aca76efe134a5d5ceae61836bce657d5d99ebc
Ethan H <jujugregoryh@gmail.com>
2026-03-31 18:49:19 -0400

    simplify planning. - remove planned(Id, Credit, Sem) and use another planned_credit(Id, Credit) for the credit option. this makes constraints looked better, since the credit information is only used in max_credits_per_semester. also better efficiency. - parameterize planning. takes input from python. - add another file with extra 'when' removed from aggregation to compare   performance.

--- run_clingo.py
+++ run_clingo.py
@@ -6,6 +6,9 @@ from pprint import pprint
 
 DEFAULT_MAIN_LP = 'cse_req_clingo.lp'
 DEFAULT_KB_LP = 'kb_complete.lp'
+MIN_SEM = (2024, 2)
+NUM_SEMS = 8
+NUM_CREDITS_PER_SEM = 18
 
 class ClingoContext:
   ## can't have python code in the clingo file if using python api.
@@ -16,14 +19,31 @@ class ClingoContext:
   def course_prog(self, course_id):
     return clingo.String(course_id.string[:3])
 
-def print_clingo_stats(stats: clingo.control.Control.statistics):
-  total_time = stats['summary']['times']['total']
-  solve_time = stats['summary']['times']['solve']
+def print_clingo_stats(stats):
+  times = stats.get('summary', {}).get('times', {})
+  total_time = times.get('total', 0)
+  solve_time = times.get('solve', 0)
   ground_time = total_time - solve_time
-  
-  print(f"\nTotal time: {total_time:.4f} seconds\n"
-        f"Grounding time: {ground_time:.4f} seconds\n"
-        f"Solving time: {solve_time:.4f} seconds\n")
+  print("======================")
+  print(f"Total time:     {total_time:.4f} seconds")
+  print(f"Grounding time: {ground_time:.4f} seconds")
+  print(f"Solving time:   {solve_time:.4f} seconds")
+  lp_stats = stats.get('problem', {}).get('lp', {})
+  if lp_stats:
+    print(f"Atoms (Variables): {int(lp_stats.get('atoms', 0)):,}")
+    print(f"Generated Rules:   {int(lp_stats.get('rules', 0)):,}")
+    print(f"Rule Bodies:       {int(lp_stats.get('bodies', 0)):,}")
+    print(f"Equivalences:      {int(lp_stats.get('eqs', 0)):,}")
+  solving_stats = stats.get('solving', {}).get('solvers', {})
+  if solving_stats:
+    choices = int(solving_stats.get('choices', 0))
+    conflicts = int(solving_stats.get('conflicts', 0))
+    restarts = int(solving_stats.get('restarts', 0))
+    
+    print(f"Choices:   {choices:,}")
+    print(f"Conflicts: {conflicts:,}")
+    print(f"Restarts:  {restarts:,}")
+  print("======================")
 
 def run_clingo(mode, main_lp, kb_lp, taken_set = set()):
   ## runs Clingo, injects the taken courses, and returns the checked dict and schedule.
@@ -38,17 +58,37 @@ def run_clingo(mode, main_lp, kb_lp, taken_set = set()):
           'sci', 'ethics', 'writing', 'credits_at_SB',
           'degree')   ## include degree as an item
 
+  min_sem = min(c.when for c in taken_set) if taken_set else MIN_SEM
+  max_sem = max(c.when for c in taken_set) if taken_set else MAX_SEM
+
+  def get_sem_distance(sem1: tuple, sem2: tuple): ## sem1, sem2 are (year, term) tuples
+    return (sem1[0] - sem2[0]) * 4 + (sem1[1] - sem2[1])
+
+  def sem_to_int(sem: tuple):
+    nonlocal min_sem
+    return get_sem_distance(sem, min_sem) + 1
+
+  def int_to_sem(sem_int: int):
+    nonlocal min_sem
+    distance = sem_int - 1
+    year = min_sem[0] + (min_sem[1] - 1 + distance) // 4
+    term = (min_sem[1] - 1 + distance) % 4 + 1
+    return (year, term)
+
   test_facts = ""
   for c in taken_set:
     ## get taken facts from test.
-    test_facts += f'taken("{c.id}", {c.credits}, "{c.grade}", {str(c.when)}, "{c.where}").\n'
+    test_facts += f'taken("{c.id}", {c.credits}, "{c.grade}", {sem_to_int(c.when)}, "{c.where}").\n'
     if mode == 'plan':
       test_facts += f'taken_id("{c.id}").\n'  ## taken_id is only for planning, not needed in checking
-
-  ctrl.add("base", [], test_facts)
+  
+  ctrl.add("input", [], test_facts)
   
   to_ground = [("base", []), ("input", []), ("check", [])]
-  if mode == 'plan': to_ground.append(("plan", []))
+  if mode == 'plan': 
+    start_sem = sem_to_int(max_sem) + 1
+    finish_sem = start_sem + NUM_SEMS
+    to_ground.append(("plan", [clingo.Number(start_sem), clingo.Number(finish_sem), clingo.Number(NUM_CREDITS_PER_SEM)]))
 
   ctrl.ground(to_ground, context=ClingoContext())
   
@@ -70,15 +110,9 @@ def run_clingo(mode, main_lp, kb_lp, taken_set = set()):
         item = str(sym.arguments[0])
         checked[item][0] = True
       elif sym.name == "planned":             ## planning mode
-        cid_sym, credit_sym_, semester_sym = sym.arguments
+        cid_sym, semester_sym = sym.arguments
         cid = str(cid_sym).strip('"')
-        ## convert semester symbol from clingo to python
-        if semester_sym.type == clingo.SymbolType.Number:    ## previous: just number for semester
-          sem = semester_sym.number
-        elif semester_sym.type == clingo.SymbolType.Function and len(semester_sym.arguments) == 2: ## (year, term) tuple for semester
-          sem = (semester_sym.arguments[0].number, semester_sym.arguments[1].number)
-        else:
-          raise ValueError(f"Unexpected semester symbol: {semester_sym}")
+        sem = int_to_sem(semester_sym.number)
         planned_courses[cid] = sem            ## record planned semester
         schedule[sem].append(cid)             ## add course to schedule
 
@@ -88,7 +122,7 @@ def run_clingo(mode, main_lp, kb_lp, taken_set = set()):
         if item == "credits_at_SB":  ## val.name is items123 or items23. matches python witness for credits_at_SB
           checked[item][1].append(f"{val.name} = {val.arguments[0].number}")
           continue
-        course = str(sym.arguments[1]).strip('"')
+        course = str(val).strip('"')
         if course in planned_courses:         ## for planned courses, indicate the semester
           course += f' (sem {planned_courses[course]})'
         checked[item][1].append(course)
@@ -112,7 +146,6 @@ def run_clingo(mode, main_lp, kb_lp, taken_set = set()):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Run the Degree Checker and Planner.")
   parser.add_argument('-m', '--mode', choices=['check', 'plan'], default='check', help="Run mode.")
-  # parser.add_argument('-t', '--taken', nargs='+', default=[], help="List of taken courses (e.g. 'CSE 114' 'CSE 214').")
   parser.add_argument('-f', '--file', default=DEFAULT_MAIN_LP, help="Path to the main .lp file that encodes the logic.")
   parser.add_argument('-k', '--kb', default=DEFAULT_KB_LP, help="Path to the KB .lp file.")
   
